7359b5be9617a60864783c426d4c9b22f6ff8983
tgray017@gmail.com <tgray017@gmail.com>
2018-09-11 22:33:00 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,8 @@ WIN_COMBINATIONS = [
 
 
 def won?(board)
-  WIN_COMBINATIONS.each do |combination|
+  combination = 0
+  while combination < WIN_COMBINATIONS.length
     current_combination = WIN_COMBINATIONS[combination]
     xwin = current_combination.all? {|position| board[position] == "X"}
     owin = current_combination.all? {|position| board[position] == "O"}

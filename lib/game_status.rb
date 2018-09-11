0c974ff28a58d31c8ce36f339ae4beac32a91e17
tgray017@gmail.com <tgray017@gmail.com>
2018-09-11 22:01:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,7 +38,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if won?(board) == false && full?(board) == true
+  if (won?(board) == false || won?(board) == nil)  && full?(board) == true
     true
   else
     false
